1d895bfc763436de9cdfe90015da2693dddecad8
flygoinggoing <929475257@qq.com>
2017-07-04 15:24:03 +0800

    去重

---
+++ segment/seg_dir_in_one.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import os
+import jieba
+
+def run(file_document_path,write_path):
+    # 在linux中地址不用转码的
+    file_document_path = unicode(file_document_path, "utf8")
+    write_path = unicode(write_path, "utf8")
+
+    file_list = os.listdir(file_document_path)
+    with open(write_path,'a+') as wf:
+        for file in file_list:
+            # print file.decode('gbk')  # window
+            # print file  # linux
+            print(file+':processing......')
+            with open(file_document_path+file) as rf:
+                for line in rf:
+                    if line.startswith('</doc>') or line.startswith('<doc>'):
+                        continue
+                    seg_list = jieba.cut(line)
+                    list = []
+                    for word in seg_list:
+                        if (not word.__eq__(' ')) and (not word.__eq__('　') and (not word.__eq__('\t'))):
+                            list.append(word)
+                    wf.write(' '.join(list))
+    pass
+
+if __name__ == '__main__':
+    ## windows
+    # run('D:/NLP/语料/sogouT/test/','D:/NLP/语料/sogouT/test.merge')
+
+    ## linux
+    run('/home/guanpf/语料/sogouT/segment/','/home/guanpf/语料/sogouT/corpus.all')
